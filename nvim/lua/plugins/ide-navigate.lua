77dbf3db170fcf6cb608b863e8bbf160783a93e8
moua <moua@moua-Latitude-5480-of-lolaitu@linux-casser.fr>
2025-03-11 19:10:13 +0100

    new neovim config v1

---
+++ nvim/lua/plugins/ide-navigate.lua
@@ -0,0 +1,77 @@
+return {
+  --############################################################################
+  -- Telescope : fzf integrated
+  --############################################################################
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
+      'nvim-lua/plenary.nvim'
+    },
+    config = function()
+     local builtin = require("telescope.builtin")
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    end
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+    end
+  },
+
+  --############################################################################
+  -- File navigation menu on side
+  --############################################################################
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+      --"3rd/image.nvim"
+    },
+    config = function()
+      vim.keymap.set('n', '<leader>ft', ':Neotree filesystem reveal right toggle dir=%:p:h<CR>', {})
+    end
+  },
+
+  --############################################################################
+  -- opened buffer bar on top
+  --############################################################################
+  {
+    "akinsho/bufferline.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    version = "*",
+    opts = {
+      options = {
+        separator_style = "slant",
+        offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+      },
+    },
+  },
+
+  --############################################################################
+  -- bottom status line 
+  --############################################################################
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup({
+        options = {
+          theme = 'dracula'
+        }
+      })
+    end
+  },
+}
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,7 +1,24 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set nocompatible")           --disable compatibility to old-time vi
+vim.cmd("set showmatch")              --show matching 
+vim.cmd("set ignorecase")             --case insensitive 
+vim.cmd("set mouse=v")                --middle-click paste with 
+vim.cmd("set hlsearch")               --highlight search 
+vim.cmd("set incsearch")              --incremental search
+vim.cmd("set expandtab")              --converts tabs to white space
+vim.cmd("set tabstop=2")              --number of columns occupied by a tab 
+vim.cmd("set softtabstop=2")          --see multiple spaces as tabstops so <BS> does the right thing
+vim.cmd("set shiftwidth=2")           --width for autoindents
+vim.cmd("set autoindent")             --indent a new line the same amount as the line just typed
+vim.cmd("set wildmode=longest,list")  --get bash-like tab completions
+vim.cmd("set number")                 --add line numbers
+vim.cmd("set relativenumber")         --line number are relative to cursor position
+vim.cmd("set cc=80")                  --set an 80 column border for good coding style
+vim.cmd("filetype plugin indent on")  --allow auto-indenting depending on file type
+vim.cmd("syntax on")                  --syntax highlighting
+vim.cmd("set mouse=a")                --enable mouse click
+vim.cmd("set clipboard=unnamedplus")  --using system clipboard
+vim.cmd("filetype plugin on")
+vim.cmd("set cursorline")             --highlight current cursorline
 
 vim.g.mapleader = " "
 
